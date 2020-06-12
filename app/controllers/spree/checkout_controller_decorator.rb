fd9495c90d7876e2c10375703c1d8ff2e67f4ad6
Blue-Lamp <pelekhvolodymyr@hotmail.com>
2020-06-12 14:08:47 +0300

    update CheckoutControllerDecorator

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -1,12 +1,13 @@
-module Spree
-  if defined? Spree::Frontend
+# frozen_string_literal: true
+
+if defined? Spree::Frontend
+  module Spree
     module CheckoutControllerDecorator
       # Override because we don't want to remove unshippable items from the order
       # A bundle itself is an unshippable item
       def before_payment; end
     end
   end
+
+  Spree::CheckoutController.prepend Spree::CheckoutControllerDecorator
 end
-
-Spree::CheckoutController.prepend Spree::CheckoutControllerDecorator
-
